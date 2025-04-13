e6c5181ad41639e4c32090cf512a685c899214ab
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-04-13 19:02:52 -0300

    Criação da classe Autor Model

---
+++ src/main/java/com/github/lucasdevrj/bibliotech/AutorModel.java
@@ -0,0 +1,12 @@
+package com.github.lucasdevrj.bibliotech;
+
+import java.time.LocalDate;
+
+public class AutorModel {
+
+    private Long id;
+    private String nome;
+    private String nacionalidade;
+    private LocalDate dataNascimento;
+    private String biografia;
+}
