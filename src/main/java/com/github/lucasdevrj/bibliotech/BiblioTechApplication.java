3e93198253a0a6976d12a91f2e6370834de6856f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-04-13 14:40:54 -0300

    Projeto inicializado.

---
+++ src/main/java/com/github/lucasdevrj/bibliotech/BiblioTechApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucasdevrj.bibliotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lucasdevrj/bibliotech/BiblioTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucasdevrj.bibliotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
