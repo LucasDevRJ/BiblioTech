f661defe7a96f3f835295c1499bf87e67d923bf3
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-04-13 16:06:55 -0300

    Adição de anotações e primeiro método.

--- src/main/java/com/github/lucasdevrj/bibliotech/Controller.java
+++ src/main/java/com/github/lucasdevrj/bibliotech/Controller.java
@@ -1,4 +1,15 @@
 package com.github.lucasdevrj.bibliotech;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class Controller {
+
+    @GetMapping("/welcome")
+    public String displaysWelcomeMessage() {
+        return "Seja bem-vindo a BiblioTech!";
+    }
 }
