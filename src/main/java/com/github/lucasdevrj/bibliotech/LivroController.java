a87025122f222e9dc6a71ccd0605229772d1b544
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-04-13 18:17:32 -0300

    Alteração no nome da classe.

--- src/main/java/com/github/lucasdevrj/bibliotech/LivroController.java
+++ src/main/java/com/github/lucasdevrj/bibliotech/LivroController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class LivroController {
 
     @GetMapping("/welcome")
     public String displaysWelcomeMessage() {
