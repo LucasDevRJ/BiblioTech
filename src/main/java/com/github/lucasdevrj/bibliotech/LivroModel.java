efe13f600281c384220b0c9295a0eb3f8a09ef25
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-04-13 21:41:53 -0300

    Mais anotações

--- src/main/java/com/github/lucasdevrj/bibliotech/LivroModel.java
+++ src/main/java/com/github/lucasdevrj/bibliotech/LivroModel.java
@@ -1,4 +1,35 @@
 package com.github.lucasdevrj.bibliotech;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "livros")
 public class LivroModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String resumo;
+    private int numeroPaginas;
+    private String isbn;
+    private LocalDate dataPublicacao;
+    private CategoriaModel categoria;
+    private AutorModel autor;
+    private String idioma;
+
+    public LivroModel() {
+    }
+
+    public LivroModel(String titulo, AutorModel autor, CategoriaModel categoria, LocalDate dataPublicacao, String isbn, int numeroPaginas, String idioma) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.categoria = categoria;
+        this.dataPublicacao = dataPublicacao;
+        this.isbn = isbn;
+        this.numeroPaginas = numeroPaginas;
+        this.idioma = idioma;
+    }
 }
