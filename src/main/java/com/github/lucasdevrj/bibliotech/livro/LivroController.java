35016e4c27d39c0388239b6150daefe27cb5cdbb
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-04-20 16:13:40 -0300

    feat: mock de endpoints.

--- src/main/java/com/github/lucasdevrj/bibliotech/livro/LivroController.java
+++ src/main/java/com/github/lucasdevrj/bibliotech/livro/LivroController.java
@@ -1,8 +1,6 @@
 package com.github.lucasdevrj.bibliotech.livro;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,35 @@ public class LivroController {
     @GetMapping("/welcome")
     public String displaysWelcomeMessage() {
         return "Seja bem-vindo a BiblioTech!";
+    }
+
+    //Adicionar Livro (CREATE)
+    @PostMapping("/adicionar")
+    public String adicionarLivro() {
+        return "Livro adicionado com sucesso!";
+    }
+
+    //Exibir todos os Livros (READ)
+    @GetMapping("/listar")
+    public String listarLivros() {
+        return "Livros listados com sucesso!";
+    }
+
+    //Exibir Livro por ID (READ)
+    @GetMapping("/exibirPorId")
+    public String exibirLivroPorId() {
+        return "Livro exibido com sucesso!";
+    }
+
+    //Alterar dados dos Livros (UPDATE)
+    @PutMapping("/alterarLivroPorId")
+    public String alterarLivroPorId() {
+        return "Livro alterado com sucesso!";
+    }
+
+    //Deletar Livro (DELETE)
+    @DeleteMapping("/deletarLivroPorId")
+    public String deletarLivroPorId() {
+        return "Livro deletado com sucesso!";
     }
 }
