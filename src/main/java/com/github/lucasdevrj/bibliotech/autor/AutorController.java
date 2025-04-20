f290d0f54749a51690843e9fd296889e961b36e5
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-04-20 17:47:03 -0300

    feat: Controller do Autor.

--- src/main/java/com/github/lucasdevrj/bibliotech/autor/AutorController.java
+++ src/main/java/com/github/lucasdevrj/bibliotech/autor/AutorController.java
@@ -1,4 +1,33 @@
 package com.github.lucasdevrj.bibliotech.autor;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/autores")
 public class AutorController {
+
+    @PostMapping("/adicionar")
+    public String adicionar() {
+        return "Autor adicionado com sucesso!";
+    }
+
+    @GetMapping("/listar")
+    public String listar() {
+        return "Autores listados com sucesso!";
+    }
+
+    @GetMapping("/exibirPorId")
+    public String exibirPorId() {
+        return "Autor exibido com sucesso!";
+    }
+
+    @PutMapping("/atualizarPorId")
+    public String atualizarPorId() {
+        return "Autor atualizado com sucesso!";
+    }
+
+    @DeleteMapping("/deletarPorId")
+    public String deletarPorId() {
+        return "Autor deletado com sucesso!";
+    }
 }
