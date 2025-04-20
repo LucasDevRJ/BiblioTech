4764f9758022da0f31293dba0526d133b52c83ac
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-04-20 17:34:19 -0300

    feat: mock de endpoint da Categoria

--- src/main/java/com/github/lucasdevrj/bibliotech/categoria/CategoriaController.java
+++ src/main/java/com/github/lucasdevrj/bibliotech/categoria/CategoriaController.java
@@ -1,4 +1,34 @@
 package com.github.lucasdevrj.bibliotech.categoria;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/categorias")
 public class CategoriaController {
+
+    @PostMapping("/adicionar")
+    public String adicionar() {
+        return "Categoria adicionada com sucesso!";
+    }
+
+    @GetMapping("/listar")
+    public String listar() {
+        return "Categoria listada com sucesso!";
+    }
+
+    @GetMapping("/exibirPorId")
+        public String exibirPorId() {
+        return "Categoria exibida com sucesso!";
+    }
+
+    @PutMapping("/atualizarPorId")
+    public String atualizarPorId() {
+        return "Categoria atualizada com sucesso!";
+    }
+
+    @DeleteMapping("/deletarPorId")
+    public String deletarPorId() {
+        return "Categoria deletada com sucesso!";
+    }
+
 }
