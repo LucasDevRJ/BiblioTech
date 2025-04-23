a9488328e214de6edbc747f453f556ddd5f73076
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-04-23 17:59:48 -0300

    feat: copiando os atributos, construtores e encapsulamento da classe Livro Model.

--- src/main/java/com/github/lucasdevrj/bibliotech/livro/LivroDTO.java
+++ src/main/java/com/github/lucasdevrj/bibliotech/livro/LivroDTO.java
@@ -1,4 +1,24 @@
 package com.github.lucasdevrj.bibliotech.livro;
+import com.github.lucasdevrj.bibliotech.autor.AutorModel;
+import com.github.lucasdevrj.bibliotech.categoria.CategoriaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class LivroDTO {
+
+    private Long id;
+    private String titulo;
+    private String resumo;
+    private int numeroPaginas;
+    private String isbn;
+    private LocalDate dataPublicacao;
+    private CategoriaModel categoria;
+    private AutorModel autor;
+    private String idioma;
 }
