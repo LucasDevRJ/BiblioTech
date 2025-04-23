3c090858e4e22a775e7c769a2be494b0beb4142c
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-04-23 18:21:55 -0300

    feat: mapeando o DTO com o Model de Livro.

--- src/main/java/com/github/lucasdevrj/bibliotech/livro/LivroMapper.java
+++ src/main/java/com/github/lucasdevrj/bibliotech/livro/LivroMapper.java
@@ -1,4 +1,33 @@
 package com.github.lucasdevrj.bibliotech.livro;
 
 public class LivroMapper {
+
+    public LivroModel map(LivroDTO livroDTO) {
+        LivroModel livroModel = new LivroModel();
+        livroModel.setId(livroDTO.getId());
+        livroModel.setTitulo(livroDTO.getTitulo());
+        livroModel.setResumo(livroDTO.getResumo());
+        livroModel.setNumeroPaginas(livroDTO.getNumeroPaginas());
+        livroModel.setIsbn(livroDTO.getIsbn());
+        livroModel.setDataPublicacao(livroDTO.getDataPublicacao());
+        livroModel.setCategoria(livroDTO.getCategoria());
+        livroModel.setAutor(livroDTO.getAutor());
+        livroModel.setIdioma(livroDTO.getIdioma());
+        return livroModel;
+    }
+
+    public LivroDTO map(LivroModel livroModel) {
+        LivroDTO livroDTO = new LivroDTO();
+        livroDTO.setId(livroModel.getId());
+        livroDTO.setTitulo(livroModel.getTitulo());
+        livroDTO.setResumo(livroModel.getResumo());
+        livroDTO.setNumeroPaginas(livroModel.getNumeroPaginas());
+        livroDTO.setIsbn(livroModel.getIsbn());
+        livroDTO.setDataPublicacao(livroModel.getDataPublicacao());
+        livroDTO.setCategoria(livroModel.getCategoria());
+        livroDTO.setAutor(livroModel.getAutor());
+        livroDTO.setIdioma(livroModel.getIdioma());
+        return livroDTO;
+    }
+
 }
