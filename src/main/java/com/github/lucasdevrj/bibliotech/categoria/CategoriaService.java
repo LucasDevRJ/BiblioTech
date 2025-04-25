db7dbba0a07ecbf192ee3ba5a7452dbe641a099d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-04-25 20:39:05 -0300

    feat: refatorando a classe Service e Controller adicionando o DTO e Mapper.

--- src/main/java/com/github/lucasdevrj/bibliotech/categoria/CategoriaService.java
+++ src/main/java/com/github/lucasdevrj/bibliotech/categoria/CategoriaService.java
@@ -4,37 +4,46 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoriaService {
 
     private CategoriaRepository categoriaRepository;
+    private CategoriaMapper categoriaMapper;
 
-    public CategoriaService(CategoriaRepository categoriaRepository) {
+    public CategoriaService(CategoriaRepository categoriaRepository, CategoriaMapper categoriaMapper) {
         this.categoriaRepository = categoriaRepository;
+        this.categoriaMapper = categoriaMapper;
     }
 
-    public List<CategoriaModel> listarTodasAsCategorias() {
-        return categoriaRepository.findAll();
+    public List<CategoriaDTO> listarTodasAsCategorias() {
+        List<CategoriaModel> categorias = categoriaRepository.findAll();
+        return categorias.stream().map(categoriaMapper::map).collect(Collectors.toList());
     }
 
-    public CategoriaModel exibirCategoriaPorId(Long id) {
-        Optional<CategoriaModel> categoria = categoriaRepository.findById(id);
-        return categoria.orElse(null);
+    public CategoriaDTO exibirCategoriaPorId(Long id) {
+        Optional<CategoriaModel> categoriaPorId = categoriaRepository.findById(id);
+        return categoriaPorId.map(categoriaMapper::map).orElse(null);
     }
 
-    public CategoriaModel adicionarCategoria(CategoriaModel categoria) {
-        return categoriaRepository.save(categoria);
+    public CategoriaDTO adicionarCategoria(CategoriaDTO categoriaDTO) {
+        CategoriaModel categoria = categoriaMapper.map(categoriaDTO);
+        categoria = categoriaRepository.save(categoria);
+        return categoriaMapper.map(categoria);
     }
 
     public void deletarCategoriaPorId(Long id) {
         categoriaRepository.deleteById(id);
     }
 
-    public CategoriaModel atualizarCategoriaPorId(Long id, CategoriaModel categoriaAtualizada) {
-        if (categoriaRepository.existsById(id)) {
+    public CategoriaDTO atualizarCategoriaPorId(Long id, CategoriaDTO categoriaDTO) {
+        Optional<CategoriaModel> categoriaExistente = categoriaRepository.findById(id);
+        if (categoriaExistente.isPresent()) {
+            CategoriaModel categoriaAtualizada =  categoriaMapper.map(categoriaDTO);
             categoriaAtualizada.setId(id);
-            return categoriaRepository.save(categoriaAtualizada);
+            CategoriaModel categoriaSalva = categoriaRepository.save(categoriaAtualizada);
+            return categoriaMapper.map(categoriaSalva);
         }
         return null;
     }
